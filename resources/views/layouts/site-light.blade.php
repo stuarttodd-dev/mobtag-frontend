e0cd31fa8e4251774d887bbf1f36efe47f05e192
Stuart Todd <stuarttodd444@gmail.com>
2025-02-10 09:26:36 +0000

    refresh css

--- resources/views/layouts/site-light.blade.php
+++ resources/views/layouts/site-light.blade.php
@@ -15,7 +15,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <link href="{{ asset('assets/css/prism.css') }}" rel="stylesheet" />
-        <link href="{{ asset('static/css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('static/css/style.css?=' . time()) }}" rel="stylesheet">
 
         @yield('styles')
 
